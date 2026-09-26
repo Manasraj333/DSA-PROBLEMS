e9193104dfb04277b9b52616727ded3909dd8c02
Manascodes247 <manasraj362@gmail.com>
2026-09-26 19:55:48 +0530

    Time: 4 ms (49.8%), Space: 9.4 MB (75.5%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -5,17 +5,17 @@ public:
         const int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         const string symbols[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
         
-        string roman = "";
-        
-        // Loop through the values
-        for (int i = 0; i < 13; ++i) {
-            // While the current value can fit into num, append its symbol
-            while (num >= values[i]) {
-                roman += symbols[i];
-                num -= values[i];
+
+        string roman="";
+        for(int i=0;i<13;i++){
+            while(num>=values[i]){
+                roman+=symbols[i];
+                num-=values[i];
+
+
             }
         }
-        
         return roman;
+       
     }
 };
